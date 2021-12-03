89d946d672b6d13e542f24409923c71ee7f5d036
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-12-03 20:59:10 +0900

    named

---
+++ named_arguments.php
@@ -0,0 +1,23 @@
+<?php
+
+class Student
+{
+    private $name;
+    private $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+}
